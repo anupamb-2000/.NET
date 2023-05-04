65d522e61214001b67b17cf0c1d34481bb9d59e9
Anupam B <anupam.b@inapp.com>
2023-05-04 15:36:45 +0530

    AppSettings Assignment

---
+++ ASP.NET Core Training/AppSettingsAssignment/AppSettingsAssignment/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppSettingsAssignment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : Controller
+    {
+        private IConfiguration _configuration;
+        public StudentController(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudentNames()
+        {
+            //var list = new List<string>();
+            var names = _configuration.GetSection("StudentNames").Get<List<string>>();
+            //foreach (var name in names)
+            //{
+            //    list.Add(name);
+            //}
+            return Ok(names);
+        }
+    }
+}
